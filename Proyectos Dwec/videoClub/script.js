d71c878bb8bc7441b61b3517ae46078e6fd2834f
Judit <jquivio1606@g.educaand.es>
2025-02-11 13:03:19 +0100

    Proyectos DWEC

--- Proyectos Dwec/videoClub/script.js
+++ Proyectos Dwec/videoClub/script.js
@@ -1,23 +1,10 @@
 const APIKEY = "13a90d5f"
 let boton = document.getElementById("btn");
 
+let carga = document.getElementById("cargando");
+
 boton.addEventListener("click", async () => {
   var tituloPelicula = document.getElementById("titulo_peli").value;
-  let contenedorDatos = document.getElementById("cont-datos");
-  let carga = document.getElementById("cargando");
-
-  let foto= document.getElementById("poster");
-
-  let titulo = document.getElementById("title");
-  let genero = document.getElementById("genre");
-  let tipo = document.getElementById("type");
-  let duracion = document.getElementById("runtime");
-  let actores = document.getElementById("actors");
-  let director = document.getElementById("director");
-  let escritor = document.getElementById("writer");
-  let idioma = document.getElementById("language");
-  let anio = document.getElementById("year");
-  let sinopsis = document.getElementById("plot");
 
   await axios.get("http://www.omdbapi.com/", {
     params: {
@@ -33,34 +20,73 @@ boton.addEventListener("click", async () => {
 
       setTimeout(() => {
         if (datos.Response == 'True') {
-
-          foto.src= datos.Poster;
-          titulo.innerHTML= "<b>Titulo: </b>"+ datos.Title;
-          genero.innerHTML= "<b>Género: </b>"+ datos.Genre;
-          tipo.innerHTML= "<b>Tipo: </b>"+ datos.Type;
-          duracion.innerHTML= "<b>Duracion: </b>"+ datos.Runtime;
-          actores.innerHTML= "<b>Actores: </b>"+ datos.Actors;
-          director.innerHTML= "<b>Director: </b>"+ datos.Director;
-          escritor.innerHTML= "<b>Escritor/es: </b>"+ datos.Writer;
-          idioma.innerHTML= "<b>Idioma: </b>"+ datos.Language;
-          anio.innerHTML= "<b>Años: </b>"+ datos.Year;
-
-          sinopsis.innerHTML= "<b>Sinopsis: </b>"+ datos.Plot;
-
-
+          mostrarInformacionPelicula(datos);
         } else {
-          contenedorDatos.innerHTML = ""; 
-          let error = document.createElement("h3");
-          error.textContent = "No se encontró la película";
-          contenedorDatos.appendChild(error);
+          mostrarError("No se encontró la película. Intenta con otro título");
         }
-
-        carga.hidden = true;
       }, 2000);
 
     })
     .catch((err) => {
-      console.log(err);
+      mostrarError("Ha ocurrido el error: " + err + ". Intentelo más tarde");
     });
 
 })
+
+let contenedorDatos = document.getElementById("cont-datos");
+
+function mostrarInformacionPelicula(datos) {
+ 
+  if (datos.Poster !== "N/A") {
+    contenedorDatos.innerHTML = `
+    <h2>${datos.Title} - (${datos.Year})</h2>
+  
+    <div class="cont1>
+      <img id="poster" class="poster" src=${datos.Poster} alt="Poster de la peli: ${datos.Title}> 
+    </div>
+
+    <div class="cont2>
+      <div id="genre" class="detalle"><strong>Género:</strong> ${datos.Genre}</div>
+      <div id="type" class="detalle"<strong>Tipo:</strong> ${datos.Type}</p>
+      <div id="runtime" class="detalle"><strong>Duración:</strong> ${datos.Runtime}</>
+      <div id="actors" class="detalle"><strong>Reparto:</strong> ${datos.Actors}</div>
+    </div>
+      <div class="cont3>
+        <div id="director" class="detalle"><strong>Director/es:</strong> ${datos.Director}</div>
+        <div id="writer" class="detalle"><strong>Escritor/es:</strong> ${datos.Writer}</div>
+        <div id="rated" class="detalle"><strong>Clasificación:</strong> ${datos.Rated}</div>
+        <div id="imdbRating" class="detalle"><strong>Calificación de los usuarios:</strong> ${datos.imdbRating}</div>
+        <div id="language" class="detalle"><strong>Idioma:</strong> ${datos.Language}</div>
+      </div>
+      <div class="cont4>
+        <div id="plot" class="detalle"><strong>Sinopsis:</strong> ${datos.Plot}</div>    
+      </div>
+    `;
+  } else {
+    contenedorDatos.innerHTML = `
+    <h2>${datos.Title} - (${datos.Year})</h2>
+      <div id="genre" class="detalle"><strong>Género:</strong> ${datos.Genre}</div>
+      <div id="type" class="detalle"<strong>Tipo:</strong> ${datos.Type}</p>
+      <div id="runtime" class="detalle"><strong>Duración:</strong> ${datos.Runtime}</>
+      <div id="actors" class="detalle"><strong>Reparto:</strong> ${datos.Actors}</div>
+      <div id="director" class="detalle"><strong>Director/es:</strong> ${datos.Director}</div>
+      <div id="writer" class="detalle"><strong>Escritor/es:</strong> ${datos.Writer}</div>
+      <div id="rated" class="detalle"><strong>Clasificación:</strong> ${datos.Rated}</div>
+      <div id="imdbRating" class="detalle"><strong>Calificación de los usuarios:</strong> ${datos.imdbRating}</div>
+      <div id="language" class="detalle"><strong>Idioma:</strong> ${datos.Language}</div>
+      <div id="plot" class="detalle"><strong>Sinopsis:</strong> ${datos.Plot}</div>
+    `;
+  }
+
+
+  carga.hidden = true;
+
+}
+
+function mostrarError(msjError) {
+  contenedorDatos.innerHTML = "";
+  let contError = document.createElement("h3");
+  contError.textContent = msjError;
+  contenedorDatos.appendChild(contError);
+}
+
