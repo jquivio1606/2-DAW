7ca86d39f8474fff788134458b6d09978e75d518
Judit Quirós Violero <judit.quiros@eviden.com>
2025-02-11 14:10:44 +0100

    Proyecto video club

--- Proyectos Dwec/videoClub/script.js
+++ Proyectos Dwec/videoClub/script.js
@@ -6,6 +6,9 @@ let carga = document.getElementById("cargando");
 boton.addEventListener("click", async () => {
   var tituloPelicula = document.getElementById("titulo_peli").value;
 
+  contenedorDatos.innerHTML = ""; // Limpia resultados anteriores
+  contenedorDatos.className = ""; // Elimina clases anteriores
+
   await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: APIKEY,
@@ -36,46 +39,44 @@ boton.addEventListener("click", async () => {
 let contenedorDatos = document.getElementById("cont-datos");
 
 function mostrarInformacionPelicula(datos) {
- 
-  if (datos.Poster !== "N/A") {
-    contenedorDatos.innerHTML = `
-    <h2>${datos.Title} - (${datos.Year})</h2>
-  
-    <div class="cont1>
-      <img id="poster" class="poster" src=${datos.Poster} alt="Poster de la peli: ${datos.Title}> 
-    </div>
 
-    <div class="cont2>
-      <div id="genre" class="detalle"><strong>Género:</strong> ${datos.Genre}</div>
-      <div id="type" class="detalle"<strong>Tipo:</strong> ${datos.Type}</p>
-      <div id="runtime" class="detalle"><strong>Duración:</strong> ${datos.Runtime}</>
-      <div id="actors" class="detalle"><strong>Reparto:</strong> ${datos.Actors}</div>
-    </div>
-      <div class="cont3>
-        <div id="director" class="detalle"><strong>Director/es:</strong> ${datos.Director}</div>
-        <div id="writer" class="detalle"><strong>Escritor/es:</strong> ${datos.Writer}</div>
-        <div id="rated" class="detalle"><strong>Clasificación:</strong> ${datos.Rated}</div>
-        <div id="imdbRating" class="detalle"><strong>Calificación de los usuarios:</strong> ${datos.imdbRating}</div>
-        <div id="language" class="detalle"><strong>Idioma:</strong> ${datos.Language}</div>
-      </div>
-      <div class="cont4>
-        <div id="plot" class="detalle"><strong>Sinopsis:</strong> ${datos.Plot}</div>    
-      </div>
-    `;
-  } else {
+  if (datos.Poster !== "N/A") {
+    contenedorDatos.classList.add("con-poster");
     contenedorDatos.innerHTML = `
-    <h2>${datos.Title} - (${datos.Year})</h2>
-      <div id="genre" class="detalle"><strong>Género:</strong> ${datos.Genre}</div>
-      <div id="type" class="detalle"<strong>Tipo:</strong> ${datos.Type}</p>
-      <div id="runtime" class="detalle"><strong>Duración:</strong> ${datos.Runtime}</>
-      <div id="actors" class="detalle"><strong>Reparto:</strong> ${datos.Actors}</div>
-      <div id="director" class="detalle"><strong>Director/es:</strong> ${datos.Director}</div>
-      <div id="writer" class="detalle"><strong>Escritor/es:</strong> ${datos.Writer}</div>
-      <div id="rated" class="detalle"><strong>Clasificación:</strong> ${datos.Rated}</div>
-      <div id="imdbRating" class="detalle"><strong>Calificación de los usuarios:</strong> ${datos.imdbRating}</div>
-      <div id="language" class="detalle"><strong>Idioma:</strong> ${datos.Language}</div>
-      <div id="plot" class="detalle"><strong>Sinopsis:</strong> ${datos.Plot}</div>
-    `;
+        <h2>${datos.Title} - (${datos.Year})</h2>
+            <img id="poster" class="poster" src="${datos.Poster}" alt="Poster de la película: ${datos.Title}">
+            <div class="div2">
+                <div class="detalle"><strong>Género:</strong> ${datos.Genre}</div>
+                <div class="detalle"><strong>Tipo:</strong> ${datos.Type}</div>
+                <div class="detalle"><strong>Duración:</strong> ${datos.Runtime}</div>
+                <div class="detalle"><strong>Reparto:</strong> ${datos.Actors}</div>
+                <div class="detalle"><strong>Director/es:</strong> ${datos.Director}</div>
+            </div>
+            <div class="cont2">
+                <div class="detalle"><strong>Escritor/es:</strong> ${datos.Writer}</div>
+                <div class="detalle"><strong>Clasificación:</strong> ${datos.Rated}</div>
+                <div class="detalle"><strong>Calificación IMDb:</strong> ${datos.imdbRating}</div>
+                <div class="detalle"><strong>Idioma:</strong> ${datos.Language}</div>
+            </div>
+            <div class="sinopsis"><strong>Sinopsis:</strong> ${datos.Plot}</div>
+        `;
+  } else {
+    contenedorDatos.classList.add("sin-poster");
+    contenedorDatos.innerHTML = `
+         <h2>${datos.Title} - (${datos.Year})</h2>
+            <div class="cont2">
+                <div class="detalle"><strong>Género:</strong> ${datos.Genre}</div>
+                <div class="detalle"><strong>Tipo:</strong> ${datos.Type}</div>
+                <div class="detalle"><strong>Duración:</strong> ${datos.Runtime}</div>
+                <div class="detalle"><strong>Reparto:</strong> ${datos.Actors}</div>
+                <div class="detalle"><strong>Director/es:</strong> ${datos.Director}</div>
+                <div class="detalle"><strong>Escritor/es:</strong> ${datos.Writer}</div>
+                <div class="detalle"><strong>Clasificación:</strong> ${datos.Rated}</div>
+                <div class="detalle"><strong>Calificación IMDb:</strong> ${datos.imdbRating}</div>
+                <div class="detalle"><strong>Idioma:</strong> ${datos.Language}</div>
+            </div>
+            <div class="sinopsis"><strong>Sinopsis:</strong> ${datos.Plot}</div>
+        `;
   }
 
 
